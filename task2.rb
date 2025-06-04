fa3ff5b47f5c8e94670b7388f385bf87edafe362
mjegor <144266905+mjegor@users.noreply.github.com>
2025-06-04 14:23:28 +0300

    Delete task2.rb

--- task2.rb
+++
@@ -1,5 +0,0 @@
-   print "введите число: "
-   input_string = gets.chomp
-   number =input_string.to_i
-   puts "#{number+1}"
-   print "я думаю это число понравится вам больше,вспомните если денег больше вы и радуетесь больше.так и здесь"
