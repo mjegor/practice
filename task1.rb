351df963ac678bc9d15adcf74efa1bde9cb250be
egor <egor-popov2004@mail.ru>
2025-06-04 13:56:49 +0300

    первое задание

---
+++ task1.rb
@@ -0,0 +1,10 @@
+print"print your name: "
+name=gets.chomp
+
+print "print your fathername: "
+fathername=gets.chomp
+
+print "print your lastname: "
+lastname=gets.chomp
+
+puts "Hello #{name.capitalize} #{fathername.capitalize} #{lastname.capitalize}!"
